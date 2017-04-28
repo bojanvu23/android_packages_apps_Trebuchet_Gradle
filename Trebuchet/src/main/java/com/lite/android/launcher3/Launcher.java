d5cb3540171992dd3b09847cfa8f926fe7225a13
Bojan Vujasin <bojan.vujasin@comtrade.com>
2017-04-28 02:06:40 +0200

    fix import

--- Trebuchet/src/main/java/com/lite/android/launcher3/Launcher.java
+++ Trebuchet/src/main/java/com/lite/android/launcher3/Launcher.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package   com.lite.android.launcher3;
+package com.lite.android.launcher3;
 
 import android.Manifest;
 import android.animation.Animator;
